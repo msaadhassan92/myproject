e57718cfaec642e4063d60a09c17cbdf445d4508
saadhassan92 <adasaad92@gmail.com>
2019-10-09 23:26:55 -0700

    My First Commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<style type="text/css">
+	body
+	{
+		font-family: 'Nunito';
+	}
+</style>
+</head>
+<body>
+	Contact Us Page
+</body>
+</html>
